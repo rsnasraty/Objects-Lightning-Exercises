fd9c756052e8ca9b1b0d3e504d9bd56c1ca882b5
Roxanne Nasraty <rsnasraty@gmail.com>
2020-04-22 15:51:05 -0500

    Working on lightning exercises

---
+++ main.js
@@ -0,0 +1,74 @@
+// Objects = Containers for data
+/* Lightning Exercise 1: */
+
+const car = {
+    year: 2015,
+    make: "Ford",
+    model: "Mustang",
+    color: "Red"
+}
+
+console.log(car);
+
+/* Lightning Exercise 2: */
+
+const animalNames = ["Kippers", "Jack", "Gypsy", "Angus", "Seymour Bouts", "Sharky"]
+console.log(animalNames);
+
+/*
+Lightning Exercise 3: Define an object for four family members, and put each object in an array named familyMembers. Each family member object that you create should have the same keys on them, but the values will be different. */
+
+
+const dee = {
+    gender: "Female",
+    hairColor: "Blonde",
+    eyeColor: "Blue",
+    occupation: "Comedian/Bird"
+}
+
+const dennis = {
+    gender: "Male",
+    hairColor: "Blonde",
+    eyeColor: "Blue",
+    occupation: "Golden God/5 Star Man"
+}
+
+const frank = {
+    gender: "Male",
+    hairColor: "Black",
+    eyeColor: "Brown",
+    occupation: "Business Owner/Investor"
+}
+
+const barbara = {
+    gender: "Female",
+    hairColor: "Auburn",
+    eyeColor: "Blue",
+    occupation: "Haunting Dee"
+}
+
+
+const familyMembers = [dee, dennis, frank, barbara];
+
+console.log(familyMembers);
+
+/* Lightning Exercise
+Examine the object below.*/
+
+const nashvilleSoftwareSchool = {
+    founded: 2012,
+    director: "John Wark",
+    instructors: {
+        fullTime: ["Jisie", "Brenda", "Steve", "Joe", "Andy"],
+        partTime: ["Zoe", "Nathan"]
+    },
+    address: "500 Interstate Blvd. S"
+}
+Lightning Exercise 1: Output the names of the part-time instructors followed by the names of the full-time instructors in the console.
+
+const partTimeInstructors =
+
+
+
+
+Lightning Exercise 2: Output only Andy and Zoe in the console.
